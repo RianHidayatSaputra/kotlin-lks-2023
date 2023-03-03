38fe63498ae8cc21622e081c4addec0478ebc297
Zidd <nuvyazid@gmail.com>
2023-03-03 20:19:35 +0700

    test binding view

--- app/src/main/java/com/example/latihan1/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/latihan1/adapter/PostAdapter.kt
@@ -9,8 +9,7 @@ import com.example.latihan1.databinding.ActivityItemsBinding
 import com.example.latihan1.responses.PostResponse
 
 class PostAdapter(private val list: ArrayList<PostResponse>):RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
-    private lateinit var binding: ActivityItemsBinding
-    inner class PostViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class PostViewHolder(private val binding: ActivityItemsBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(PostResponse: PostResponse) {
             with(itemView) {
                 val text = "id: ${PostResponse.id}\n" +
@@ -22,8 +21,8 @@ class PostAdapter(private val list: ArrayList<PostResponse>):RecyclerView.Adapte
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_posts, parent, false)
-        return PostViewHolder(view)
+        // val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_posts, parent, false)
+        return PostViewHolder(ActivityItemsBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun getItemCount(): Int = list.size
